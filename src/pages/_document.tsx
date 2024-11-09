0cb77e488aee3a819b605b832ef3b093005d17c2
Andrew Lisowski <lisowski54@gmail.com>
2024-11-08 23:12:54 -0800

    add plausible

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          defer
+          data-domain="bluesky-migrate.com"
+          src="https://plausible.io/js/script.file-downloads.outbound-links.js"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
